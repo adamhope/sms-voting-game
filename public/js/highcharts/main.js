dda653a914226c4e49fe7399ac7baef9342a212f
Adam Hope <ahope@thoughtworks.com>
2013-05-14 23:58:08 +1000

    cleanup

--- public/js/highcharts/main.js
+++ public/js/highcharts/main.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2012, Script Tutorials
  * http://www.script-tutorials.com/
  */
@@ -30,8 +30,7 @@ function ChangeChartType(chart, series, newType) {
     }
 }
 
-// Two charts definition
-var chart1, chart2;
+var chart1;
 
 // Once DOM (document) is finished loading
 $(document).ready(function() {
@@ -44,14 +43,14 @@ $(document).ready(function() {
         height: 350,
      },
      title: {
-        text: 'Tools developers plans to use to make html5 games (in %)'
+        text: 'Scoreboard'
      },
      xAxis: {
         categories: ['Processing.js', 'Impact.js', 'Other', 'Ease.js', 'Box2D.js', 'WebGL', 'DOM', 'CSS', 'Canvas', 'Javascript']
      },
      yAxis: {
         title: {
-           text: 'Interviewed'
+           text: 'Points'
         }
      },
      series: [{
@@ -66,52 +65,8 @@ $(document).ready(function() {
      }]
     });
 
-    // Second chart initialization (pie chart)
-    chart2 = new Highcharts.Chart({
-        chart: {
-            renderTo: 'chart_2',
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            height: 350,
-        },
-        title: {
-            text: 'Pie chart diagram for the first developer'
-        },
-        tooltip: {
-            pointFormat: '<b>{point.percentage}%</b>',
-            percentageDecimals: 1
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: false
-                },
-                showInLegend: true
-            }
-        },
-         series: [{
-         type: 'pie',
-            name: 'Dev #1',
-            data: [
-                ['Processing.js', 5],
-                ['Impact.js', 10],
-                ['Other', 20],
-                ['Ease.js', 22],
-                ['Box2D.js', 25],
-                ['WebGL', 28],
-                ['DOM', 30],
-                ['CSS', 40],
-                ['Canvas', 80],
-                ['Javascript', 90]
-            ]
-         }]
-    });
-
     // Switchers (of the Chart1 type) - onclick handler
-    $('.switcher').click(function () {  
+    $('.switcher').click(function () {
         var newType = $(this).attr('id');
         ChangeChartType(chart1, chart1.series, newType);
     });
