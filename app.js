8bcac19163953ced5001f45af0651a3801dd959d
Adam Hope <ahope@thoughtworks.com>
2013-05-14 21:01:44 +1000

    lots of reorganising and sample d3 force directed graph

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express'),
   routes = require('./routes'),
   participants = require('./routes/participants'),
-  http = require('http'), 
+  http = require('http'),
+  force_directed = require('./routes/force_directed'),
+  http = require('http'),
   path = require('path'),
   mongoose = require('mongoose');
 
@@ -25,7 +27,9 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+
 app.get('/participants', participants.list);
+app.get('/force_directed', force_directed.force_directed);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/force_directed.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET home page.
+ */
+
+exports.force_directed = function(req, res){
+  res.render('force_directed', {title: "force directed"});
+};
