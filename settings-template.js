fd0c63173a3bed7e14737e05fb081daf0da3bafc
Frederic Lemay <frederic.lemay@gmail.com>
2013-05-29 19:10:54 +1000

    templating settings

---
+++ settings-template.js
@@ -0,0 +1,8 @@
+// This is just a template. Do not check-in your real credentials
+var settings = {
+  url: 'www.sms-url.com',
+  key: 'ab12cd34ef56',
+  secret: 'thisisasecret'
+};
+
+module.exports = settings;
