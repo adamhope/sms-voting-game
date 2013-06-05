4afe99074831d0783670fe8f6da45a043e4560d4
Adam Hope <ahope@thoughtworks.com>
2013-06-05 17:47:20 +1000

    refactoring

--- public/js/d3/force_directed_directional.js
+++ public/js/d3/force_directed_directional.js
@@ -1,3 +1,30 @@
+// TODO
+// - more refactoring
+// - dynamically add nodes
+// - update appearnce of node as number of connections increases
+// - find out where undefined node comes from...
+
+// SVG Document Structure
+// - SVG
+//   - defs
+//     marker
+//     licensing
+//     resolved
+//   - g
+//      paths
+//      ...
+//      ...
+//   - g
+//      circles
+//      ...
+//      ...
+//   - g
+//      - g
+//        text
+//        text
+//      ...
+//      ...
+
 var dashboard = (function () {
 
   var nodes = {},
@@ -5,100 +32,122 @@ var dashboard = (function () {
         w: 960,
         h: 500
       },
-      links;
+      force, links, svg,
+      circle, path, text;
 
-  function draw(data) {
+  // Compute the distinct nodes from the links.
+  // NOTE this is just updating the dataset. Nothing to do with re-drawing the graph?
+  function addLink(link) {
+    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+  }
+
+  function init(data) {
 
     links = data.links;
 
-    // Compute the distinct nodes from the links.
-    links.forEach(function(link) {
-      link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
-      link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
-    });
+    links.forEach(addLink);
 
-    var force = d3.layout.force()
-        .nodes(d3.values(nodes))
-        .links(links)
-        .size([cfg.w, cfg.h])
-        .linkDistance(60)
-        .charge(-300)
-        .on("tick", tick)
-        .start();
+    svg = d3.select('body').append('svg:svg')
+      .attr('width', cfg.w)
+      .attr('height', cfg.h);
 
-    var svg = d3.select("body").append("svg:svg")
-        .attr("width", cfg.w)
-        .attr("height", cfg.h);
+    force = d3.layout.force()
+      .nodes(d3.values(nodes))
+      .links(links)
+      .size([cfg.w, cfg.h])
+      .linkDistance(90)
+      .charge(-300)
+      .start();
 
     // Per-type markers, as they don't inherit styles.
-    svg.append("svg:defs").selectAll("marker")
-        .data(["suit", "licensing", "resolved"])
-      .enter().append("svg:marker")
-        .attr("id", String)
-        .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 15)
-        .attr("refY", -1.5)
-        .attr("markerWidth", 6)
-        .attr("markerHeight", 6)
-        .attr("orient", "auto")
-      .append("svg:path")
-        .attr("d", "M0,-5L10,0L0,5");
+    svg.append('svg:defs').selectAll('marker')
+        // TODO change this to ['participant']
+        .data(['suit', 'licensing', 'resolved'])
+      .enter().append('svg:marker')
+        .attr('id', String)
+        .attr('viewBox', '0 -5 10 10')
+        .attr('refX', 15)
+        .attr('refY', -1.5)
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('orient', 'auto')
+      .append('svg:path')
+        .attr('d', 'M0,-5L10,0L0,5');
 
-    var path = svg.append("svg:g").selectAll("path")
-        .data(force.links())
-      .enter().append("svg:path")
-        .attr("class", function(d) { return "link " + d.type; })
-        .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
-
-    var circle = svg.append("svg:g").selectAll("circle")
-        .data(force.nodes())
-      .enter().append("svg:circle")
-        .attr("r", 6)
-        .call(force.drag);
-
-    var text = svg.append("svg:g").selectAll("g")
-        .data(force.nodes())
-      .enter().append("svg:g");
-
-    // A copy of the text with a thick white stroke for legibility.
-    text.append("svg:text")
-        .attr("x", 8)
-        .attr("y", ".31em")
-        .attr("class", "shadow")
-        .text(function(d) { return d.name; });
-
-    text.append("svg:text")
-        .attr("x", 8)
-        .attr("y", ".31em")
-        .text(function(d) { return d.name; });
-
-    // Use elliptical arc path segments to doubly-encode directionality.
-    function tick() {
-      path.attr("d", function(d) {
+    force.on('tick', function (e) {
+      path.attr('d', function(d) {
         var dx = d.target.x - d.source.x,
             dy = d.target.y - d.source.y,
             dr = Math.sqrt(dx * dx + dy * dy);
-        return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+        return 'M' + d.source.x + ',' + d.source.y + 'A' + dr + ',' + dr + ' 0 0,1 ' + d.target.x + ',' + d.target.y;
       });
 
-      circle.attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
+      circle.attr('transform', function(d) {
+        return 'translate(' + d.x + ',' + d.y + ')';
       });
 
-      text.attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
+      text.attr('transform', function(d) {
+        return 'translate(' + d.x + ',' + d.y + ')';
       });
+    });
+
+    update();
+
+  }
+
+
+  function update(link) {
+
+    // NOTE update dataset
+    if (link) {
+      addLink(link);
     }
+
+    // NOTE restart drawing again
+    force.start();
+
+    // --- THE ACTUAL DRAWING
+    // NOTE create an SVG container element and add paths/links
+    path = svg.append('svg:g').selectAll('path')
+        .data(force.links())
+      .enter().append('svg:path')
+        .attr('class', function(d) { return 'link ' + d.type; })
+        .attr('marker-end', function(d) { return 'url(#' + d.type + ')'; });
+
+    // NOTE Adding nodes
+    circle = svg.append('svg:g').selectAll('circle')
+        .data(force.nodes())
+      .enter().append('svg:circle')
+        .attr('r', 6)
+        .call(force.drag);
+
+    // NOTE adding node text
+    text = svg.append('svg:g').selectAll('g')
+        .data(force.nodes())
+      .enter().append('svg:g');
+
+    // A copy of the text with a thick white stroke for legibility.
+    text.append('svg:text')
+        .attr('x', 8)
+        .attr('y', '.31em')
+        .attr('class', 'shadow')
+        .text(function(d) { return d.name; });
+
+    text.append('svg:text')
+        .attr('x', 8)
+        .attr('y', '.31em')
+        .text(function(d) { return d.name; });
+
   }
 
   return {
-    links: links,
-    nodes: nodes,
-    draw: draw
+    update: update,
+    init  : init
   };
 
 }());
 
 $(document).ready(function() {
-  $.getJSON('/participants/links', dashboard.draw);
+  $.getJSON('/participants/links', dashboard.init);
 });
