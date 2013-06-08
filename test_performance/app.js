4e2993e77b3c427028e338e4c949cea50c0c94fa
Frederic Lemay <frederic.lemay@gmail.com>
2013-06-08 20:41:31 +1000

    can now configure number of workers, registrations and votes.

--- test_performance/app.js
+++ test_performance/app.js
@@ -1,29 +1,41 @@
 var Participant = require('../models/participant'),
   mongoose = require('mongoose'),
   backgrounder = require("backgrounder"),
-  async = require('async');
+  async = require('async'),
+  _ = require('underscore');
 
-var database = 'mongodb://localhost/sms-voting-game-test';
+var database = 'mongodb://localhost/sms-voting-game-test',
+  NB_WORKERS = 2,
+  NB_REGISTRATIONS_PER_WORKER = 10,
+  NB_VOTES_PER_WORKER = 100;
+
 mongoose.connect(database);
 
 Participant.remove({}, function(err) {
-  
-  var workers = initWorkers();
-  async.each(workers, function(worker, callback) {
+  async.each(initWorkers(), function(worker, callback) {
     worker.process.send(worker.message, function() {
       worker.process.terminate();
       callback();
     });
-  }, function(err){
+  }, function(err) {
     console.log('It is done!!')
     mongoose.disconnect();
   });
 
   function initWorkers() {
     var workers = [];
-    workers.push({process: backgrounder.spawn(__dirname + "/worker.js"), message: [['000', 'a000'], ['001', 'a001']]});
-    workers.push({process: backgrounder.spawn(__dirname + "/worker.js"), message: [['003', 'a003'], ['004', 'a004']]});
-
+    _(NB_WORKERS).times(function(nWorker) {
+      workers.push({
+        process: backgrounder.spawn(__dirname + "/worker.js"),
+        message: (function() {
+          var message = [];
+          _(NB_REGISTRATIONS_PER_WORKER).times(function(nRegistration) {
+            message.push([nWorker + '00' + nRegistration, nWorker + 'username' + nRegistration]);
+          });
+          return message;
+        })()
+      });
+    });
     return workers;
   }
 });
