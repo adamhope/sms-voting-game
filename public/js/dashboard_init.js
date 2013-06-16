b3ffb109ce8d7f0ad4a31386ece0ad767b5d4d86
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-06-16 15:32:14 +1000

    Added rotating of the leaderboard and the nwtwork graph

--- public/js/dashboard_init.js
+++ public/js/dashboard_init.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  $("#leaderboard").hide();
   $.getJSON('/participants/links', initGraph);
   $.getJSON('/participants/json', updateLeaderboard);
 });
@@ -7,4 +8,17 @@ $(document).ready(function() {
 setInterval(function () {
   $.getJSON('/participants/links', updateGraph);
   $.getJSON('/participants/json', updateLeaderboard);
+}, 2000);
+
+
+
+setInterval(function() { 
+
+  if ($("#network-graph").is(":visible")) {
+    $("#network-graph").hide();
+    $("#leaderboard").show();
+  } else {
+    $("#leaderboard").hide();
+    $("#network-graph").show();
+  }
 }, 2000);
