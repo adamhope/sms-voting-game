57ea8708db5ee8164d0e479ed62f7cbd41ad7a99
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-06-17 19:49:56 +1000

    Changed force parameters

--- public/js/network_graph.js
+++ public/js/network_graph.js
@@ -153,8 +153,8 @@ function myGraph(el) {
 
     force
       .gravity(0.06)
-      .charge(function(d) { return d._children ? 0 : -100 ;}) // -200
-      .linkDistance(function(d) { return d.target._children ? 100 : 100; }) // 100
+      .charge(function(d) { return d._children ? -100 : -200 ;}) // -200
+      .linkDistance(function(d) { return d.target._children ? 100 : 300; }) // 100
       .size([w, h])
       .start();
   };
