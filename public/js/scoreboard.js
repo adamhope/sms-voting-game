2e145b16ee49d73f7fda5414d5e291ec921d131e
Andrew Carr <acarr@thoughtworks.com>
2013-07-03 21:54:35 +1000

    restyled the scoreboard into a leaderboard/scoreboard hybrid

--- public/js/scoreboard.js
+++ public/js/scoreboard.js
@@ -1,8 +1,9 @@
 var updateScoreboard = function (data) {
   var ctx = $('#scoreboard');
-  $('.visualization-header', ctx).text('Scoreboard');
+  $('.visualization-header', ctx).append('<b class="score">&#9733;</b>Most Connected');
   $('.visualization-body', ctx).html('<ol class="all" />');
+  var topScore = data[0].score
   $(data).each(function () {
-    $('.all', ctx).append('<li><strong>' + this.score + '</strong>' + this.username + '</li>');
+    $('.all', ctx).append('<li><div class="score-bar" style="width:' + this.score/topScore*100 + '%;"></div><b class="score">' + this.score + '</b>' + this.username + '</li>');
   });
 };
